23c7ec01ff11bd3d03819284955c850195be9eb1
Salik Hussaini <47673489+salikhussaini@users.noreply.github.com>
2019-08-30 13:30:04 -0500

    Create Naive-Bayes-Classifier_Email-Similarity.py

---
+++ Naive-Bayes-Classifier_Email-Similarity.py
@@ -0,0 +1,25 @@
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import CountVectorizer
+
+emails = fetch_20newsgroups(categories = ['rec.sport.baseball', 'rec.sport.hockey']) 
+
+#print(emails.target_names)
+#print(emails.data[5])
+#print(emails.target[5])
+train_emails = fetch_20newsgroups(
+  categories = ['rec.sport.baseball', 'rec.sport.hockey'], subset = 'train', shuffle = True, random_state = 108)
+
+test_emails = fetch_20newsgroups(
+  categories = ['rec.sport.baseball', 'rec.sport.hockey'], subset = 'test', shuffle = True, random_state = 108)
+
+counter = CountVectorizer()
+counter.fit(test_emails.data + train_emails.data) 
+train_counts = counter.transform(train_emails.data)
+test_counts = counter.transform(test_emails.data)
+
+classifier = MultinomialNB()
+classifier.fit(train_counts, train_emails.target)
+
+print(classifier.score(test_counts, test_emails.target))
+
